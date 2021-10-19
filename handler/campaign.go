d1efb54e15d3a2e62afe7a3a5ddc99935b917f3c
robbinh00d <robbinhood22012@gmail.com>
2021-10-19 16:39:50 +0700

    Init project. Get Campaigns endpoint (WIP)

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,9 +1,32 @@
 package handler
 
-/*
-	Tangkap parameter di handler
-	Handler ke service
-	Serivce yg menentukan repository mana yg di call
-	repository : FindAll, FindByUserID
-	db
-*/
+import (
+	"crowfunding/campaign"
+	"crowfunding/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+
+	if err != nil {
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadGateway, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
