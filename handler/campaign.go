815c3ae0af25185c1ee287240a99dc8982e51ea7
robbinh00d <robbinhood22012@gmail.com>
2021-10-19 15:11:27 +0700

    Init project. Campaign Repository

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,9 @@
 package handler
+
+/*
+	Tangkap parameter di handler
+	Handler ke service
+	Serivce yg menentukan repository mana yg di call
+	repository : FindAll, FindByUserID
+	db
+*/
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"crowfunding/auth"
+	"crowfunding/campaign"
 	"crowfunding/handler"
 	"crowfunding/helper"
 	"crowfunding/user"
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -24,6 +26,24 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	campaignRepository := campaign.NewRepository(db)
+
+	campaigns, err := campaignRepository.FindByUserID(1)
+
+	fmt.Println("Debug")
+	fmt.Println("Debug")
+	fmt.Println("Debug")
+	fmt.Println(len(campaigns))
+
+	for _, campaign := range campaigns {
+		fmt.Println(campaign.Name)
+		if len(campaign.CampaignImages) > 0 {
+			fmt.Println("Jumlah Gambar :")
+			fmt.Println(len(campaign.CampaignImages))
+			fmt.Println(campaign.CampaignImages[0].FileName)
+		}
+	}
+
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
